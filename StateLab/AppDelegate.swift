aff912968848f04fad947610257b125510b9956d
AlexanderOkhrimenko <xanderhard@icloud.com>
2018-09-01 12:38:02 +0300

    1

---
+++ StateLab/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  StateLab
+//
+//  Created by xander on 23.12.2017.
+//  Copyright © 2017 xander. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+        [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        print(#function)
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        print(#function)
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print(#function)
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print(#function)
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+         print(#function)
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        print(#function)
+    }
+
+
+}
+
